c28b83b892a5cee246eccd6393d2c10118b2d74d
Pruce Uchiha <purushothaman21212@gmail.com>
2022-03-18 16:57:10 -0400

    design for sign up done

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,36 +1,53 @@
 import React from "react";
 import mainLogo from "../../Assets/Logo.png";
 import "./Login.scss";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div className="Login">
-      <div className="mainLogo">
-        <img className="mainLogoImage" src={mainLogo} alt="fireSpot" />
-      </div>
-      <div className="loginHeading">
-        <h2>Login to Your Account</h2>
-      </div>
-      <div className="loginComponents">
-        <div className="inputComponent">
-          <input placeholder="Email" type="text" />
+    <div className="LoginComponent">
+      <div className="Login">
+        <div className="mainLogo">
+          <img className="mainLogoImage" src={mainLogo} alt="fireSpot" />
         </div>
-        <div className="inputComponent">
-          <input placeholder="Password" type="password" />
+        <div className="loginHeading">
+          <h2>Login to Your Account</h2>
+        </div>
+        <div className="loginComponents">
+          <div className="inputComponent">
+            <input placeholder="Email" type="text" />
+          </div>
+          <div className="inputComponent">
+            <input placeholder="Password" type="password" />
+          </div>
+          <div className="buttonComponent">
+            <button> Sign in</button>
+          </div>
+          <div className="orLine">
+            <h2 className="orLineHeading">
+              <span>OR</span>
+            </h2>
+          </div>
+          <div style={{ marginTop: "20px" }} className="inputComponent">
+            <input placeholder="Phone Number" type="text" />
+          </div>
+          <div className="buttonComponent">
+            <button> Sign in Using OTP </button>
+          </div>
+        </div>
+      </div>
+      <div className="SwitchToSignup">
+        <div className="heading">
+          <h3>New Here?</h3>
+        </div>
+        <div className="paragraph">
+          <p>Sign up and discover a great</p>
+          <p>amount of new opportunities</p>
         </div>
         <div className="buttonComponent">
-          <button> Sign in</button>
-        </div>
-        <div className="orLine">
-          <h2 className="orLineHeading">
-            <span>OR</span>
-          </h2>
-        </div>
-        <div style={{ marginTop: "20px" }} className="inputComponent">
-          <input placeholder="Phone Number" type="text" />
-        </div>
-        <div className="buttonComponent">
-          <button> Sign in Using OTP </button>
+          <Link to="/signup">
+            <button>Sign up</button>
+          </Link>
         </div>
       </div>
     </div>
--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from "react";
+import mainLogo from '../../Assets/Logo.png'
+import { Link } from "react-router-dom";
+import './Signup.scss'
 
 export default function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div className="SignupComponent">
+      <div className="Signup">
+        <div className="mainLogo">
+          <img className="mainLogoImage" src={mainLogo} alt="fireSpot" />
+        </div>
+        <div className="signupHeading">
+          <h2>Create Account</h2>
+        </div>
+        <div className="signupComponents">
+          <div className="inputComponent">
+            <input placeholder="Email" type="text" />
+          </div>
+          <div className="inputComponent">
+            <input placeholder="Name" type="text" />
+          </div>
+          <div className="inputComponent">
+            <input placeholder="Password" type="password" />
+          </div>
+          <div className="inputComponent">
+            <input placeholder="Renter Password" type="password" />
+          </div>
+          <div className="inputComponent">
+            <input placeholder="Phone Number" type="text" />
+          </div>
+          <div className="buttonComponent">
+            <button> Signup</button>
+          </div>
+        </div>
+      </div>
+      <div className="SwitchToSignup">
+        <div className="heading">
+          <h3>Old User?</h3>
+        </div>
+        <div className="paragraph">
+          <p>To keep connected with us</p>
+          <p>please login with your personal info.</p>
+        </div>
+        <div className="buttonComponent">
+          <Link to="/">
+            <button>Sign in</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
